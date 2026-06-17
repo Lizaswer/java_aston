7dc79be9989b8fe6864fa347cbd202767d3e0dbf
Lyaho <lyaholizs@gmail.com>
2026-06-17 22:32:43 +0500

    исправление ошибок, добавление NPE, метод clone, добавление отдельно файла main

---
+++ src/week1/Main.java
@@ -0,0 +1,20 @@
+package week1;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> originalList = new ArrayList<>();
+        originalList.add("item1");
+        originalList.add("item2");
+
+        ImmutableClass obj1 = new ImmutableClass(1, originalList);
+
+        originalList.add("item3");
+
+        System.out.println("Список в объекте 1: " + obj1.getMutableList());
+
+        ImmutableClass obj2 = new ImmutableClass(2, null);
+        System.out.println("Список в объекте 2 (обработан null): " + obj2.getMutableList());
+    }
+}
